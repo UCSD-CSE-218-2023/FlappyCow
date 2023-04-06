1e1e66235bba38ff403806da8edde3b06bfece69
Dylan Lukes <lukes.dylan@gmail.com>
2023-04-06 12:08:45 -0700

    rename package to edu.ucsd.flappycow

---
+++ app/src/main/java/edu/ucsd/flappycow/sprites/WoodLog.java
@@ -0,0 +1,43 @@
+/**
+ * A shopped wodden log
+ * 
+ * @author Lars Harmsen
+ * Copyright (c) <2014> <Lars Harmsen - Quchen>
+ */
+
+package edu.ucsd.flappycow.sprites;
+
+import edu.ucsd.flappycow.GameActivity;
+import edu.ucsd.flappycow.GameView;
+import com.quchen.flappycow.R;
+import edu.ucsd.flappycow.Util;
+
+import android.graphics.Bitmap;
+
+public class WoodLog extends Sprite {
+
+    /**
+     * Static bitmap to reduce memory usage.
+     */
+    public static Bitmap globalBitmap;
+
+    public WoodLog(GameView view, GameActivity gameActivity) {
+        super(view, gameActivity);
+        if(globalBitmap == null){
+            globalBitmap = Util.getScaledBitmapAlpha8(gameActivity, R.drawable.log_full);
+        }
+        this.bitmap = globalBitmap;
+        this.width = this.bitmap.getWidth();
+        this.height = this.bitmap.getHeight();
+    }
+    
+    /**
+     * Sets the position
+     * @param x
+     * @param y
+     */
+    public void init(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
